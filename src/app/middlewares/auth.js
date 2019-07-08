9a77dda386409aed9d1bdeaa0cf0b611d667559f
Vinicius <vinicius.2010.s@gmail.com>
2019-07-08 19:01:44 -0300

    ADD update user method

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const headerAuthorization = req.headers.authorization;
+
+  if (!headerAuthorization)
+    return res.status(401).json({ error: 'Token not provided' });
+
+  const [, token] = headerAuthorization.split(' ');
+
+  try {
+    const decodedUserInfo = await promisify(jwt.verify)(
+      token,
+      authConfig.secret
+    );
+
+    req.userId = decodedUserInfo.id;
+
+    return next();
+  } catch (error) {
+    return res.status(401).json({ error: 'Invalid token.' });
+  }
+};
